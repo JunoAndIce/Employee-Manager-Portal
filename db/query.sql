f33b46ea9caec99a000d451178a3fc98f9bc480a
Juno & Ice <70862998+JunoAndIce@users.noreply.github.com>
2023-08-04 15:43:54 -0500

    inital

---
+++ db/query.sql
@@ -0,0 +1,5 @@
+-- Add your code below and execute file in MySQL Shell --
+
+SELECT favorite_books.book_name AS book_name, book_prices.price AS price
+FROM favorite_books
+JOIN book_prices  ON favorite_books.book_price = book_prices.id;
